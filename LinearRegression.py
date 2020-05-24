66cd6c5f73ac1db721f5dda5bfb46ab136086784
Jan Van den Schilden <jan.vandenschilden@gmail.com>
2020-05-24 16:50:13 +0200

    Finished linear regression predictor

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,4 +1,4 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 class LinearRegression:
@@ -7,8 +7,10 @@ class LinearRegression:
         self.featureCount=len(X[0])+1
         self.X = np.ones((self.sampleCount,self.featureCount))
         self.X[:,:-1]=X
-        self.params=np.zeros((3,1))
+        self.params=np.zeros((self.featureCount,1))
         self.y=y
+        self.costEvolution=np.zeros(0)
+        self.paramsEvoltion=np.zeros((0,self.featureCount))
     def gradientDescent(self,iterations=int(1e4),learningRate=1e-4):
         def MSE_partialDerivatives(X,y,weights):
             scaleFactor = -2/len(y)
@@ -31,9 +33,11 @@ class LinearRegression:
             yPredicted = X @ weights #Use inproduct to calculate predicted values
             MSE = np.sum((y-yPredicted)**2)/sampleSize
             return MSE
-        self.costEvolution=np.zeros(iterations)
-        self.paramsEvoltion=np.zeros((iterations,len(self.params)))
-        for i in range(iterations):
+        start=len(self.costEvolution)
+        self.costEvolution=np.concatenate((self.costEvolution,np.zeros(iterations)))
+        self.paramsEvoltion=np.concatenate((self.paramsEvoltion, np.zeros((iterations,len(self.params)))))
+        for j in range(iterations):
+            i =j+start
             self.params = self.params - learningRate*MSE_partialDerivatives(self.X,self.y,self.params)
             self.paramsEvoltion[i,:]=self.params[:,0]
             self.costEvolution[i]=MSE(self.X,self.y,self.params)
@@ -44,6 +48,10 @@ class LinearRegression:
         plt.title("Evolution of cost function during Gradient descent")
         plt.xlabel("iteration")
         plt.ylabel("cost")
+        xlim = (int(len(self.costEvolution)/2),len(self.costEvolution)-1)
+        ylim = (self.costEvolution[xlim[1]],self.costEvolution[xlim[0]])
+        #plt.ylim(ylim)
+        #plt.xlim(xlim)
         plt.show()
         
         plt.figure(figsize=(14,7))
@@ -54,3 +62,14 @@ class LinearRegression:
         plt.ylabel("parameter value")
         plt.legend()
         plt.show()
+    def predictTestSet(self,X_test,y_test):
+        X = np.ones((len(y_test),self.featureCount))
+        X[:,:-1] = X_test
+        y_predicted = X @ self.params
+        """ Multiply predicted with known values
+        Correctly predicted values are positive
+        wrongly predicted are negative
+        """
+        y_correct_false = y_predicted * y_test
+        ratio_correct = len(y_correct_false[y_correct_false>0])/len(y_correct_false)
+        return ratio_correct
