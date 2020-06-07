4724f031bf23c77213e5aab1e03375babd81d8b9
guillaumeBuissonChavot <57842982+guillaumeBuissonChavot@users.noreply.github.com>
2020-06-07 16:40:36 +0200

    Update merge_cyto_periplasm.py

--- merge_cyto_periplasm.py
+++ merge_cyto_periplasm.py
@@ -1,5 +1,5 @@
 with open('periplasmUniRef50.csv') as f_input_1:
-	#with open('/home/guiom/Bureau/test/cytoplasmUniRef50.csv') as f_input_2
+	#with open('cytoplasmUniRef50.csv') as f_input_2
 	with open('merge_cyto_periplasm.csv', 'w') as f_output:
 		for i in range(2001):
 			f_output.write(f_input_1.readline())
